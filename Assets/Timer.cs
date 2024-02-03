9cc3c35cffbaf97f191c62c473d1157ccce5e578
Maverick-2002 <131183332+Maverick-2002@users.noreply.github.com>
2024-02-03 23:49:38 +0530

    Timer Complete

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -19,10 +19,14 @@ public class Timer : MonoBehaviour , IPointerClickHandler
 
     [SerializeField] public Image uifill;
     [SerializeField] public Text uitext;
+    [SerializeField] public GameObject Inhale;
+    [SerializeField] public GameObject Exhale;
+    [SerializeField] public GameObject Hold;
 
     public int duration;
     public int remainingduration;
     private bool Pause;
+    public int time = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +36,7 @@ public class Timer : MonoBehaviour , IPointerClickHandler
     // Update is called once per frame
     void Update()
     {
-        
+        //Display();
     }
 
     private void being(int second)
@@ -48,11 +52,66 @@ public class Timer : MonoBehaviour , IPointerClickHandler
             uitext.text = $"{remainingduration / 60:00} : { remainingduration % 60:00}";
             uifill.fillAmount = Mathf.InverseLerp(0, duration, remainingduration);
             remainingduration --;
-            yield return new WaitForSeconds(1f);
+                if (time < 16 && time > 12)
+                {
+                    Exhale.SetActive(false);
+                    Hold.SetActive(true);
+
+                }
+                else if (time >8  && time < 12)
+                {
+                    Exhale.SetActive(true) ;
+                    Hold.SetActive(false);
+                }
+
+                else if (time > 4 && time < 8)
+                {
+                  Inhale.SetActive(false);
+                  Hold.SetActive(true);
+                }
+                else if (time > 0  && time < 4)
+                {
+                    Inhale.SetActive(true) ;
+                }
+
+                time++;
+                yield return new WaitForSeconds(1f);
         }
         yield return null;
         OnEnd();
     }
+
+   /* private void Display()
+    {
+        while (time < 17)
+        {
+            if (time < 4)
+            {
+                Inhale.SetActive(true);
+
+            }
+            else if (time > 4 && time < 8 )
+            {
+                Inhale.SetActive(false);
+                Hold.SetActive(true);
+            }
+
+            else if (time > 8 && time < 12)
+            {
+                
+                
+                Hold.SetActive(false);
+                Exhale.SetActive(true);
+            }
+            else if (time > 12 && time < 16)
+            {
+                Exhale.SetActive(false);
+                Hold.SetActive(true) ;
+            }
+            time++;
+        }
+        
+    }*/
 
     private void OnEnd()
     {
