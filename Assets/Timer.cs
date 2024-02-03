9d5bb9c9415c5a7f0d0eac4e411d6f8c43686cc8
Maverick-2002 <131183332+Maverick-2002@users.noreply.github.com>
2024-02-03 20:09:44 +0530

    Timer Update

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Michsky.UI.Heat;
 
-public class Timer : MonoBehaviour
+public class Timer : MonoBehaviour , IPointerClickHandler 
+
+   
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Pause = !Pause;
+    }
+
+
+
+
     [SerializeField] public Image uifill;
     [SerializeField] public Text uitext;
 
     public int duration;
     public int remainingduration;
+    private bool Pause;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +50,7 @@ public class Timer : MonoBehaviour
             remainingduration --;
             yield return new WaitForSeconds(1f);
         }
+        yield return null;
         OnEnd();
     }
 
